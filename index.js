1fbec8d90dc0820ca3a383430fbf189eb8371ff9
Kevin Pak <kedpak@yahoo.com>
2018-02-09 13:04:52 -0800

    Change data in object to test heroku git

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({hi: 'there'});
+	res.send({data: 'The universe'});
 });
 
 // Use port provided by Heroku or use port 5000
-
 app.listen(process.env.PORT || 5000, "0.0.0.0", function() {
 	console.log("Listening on Port");
 });
