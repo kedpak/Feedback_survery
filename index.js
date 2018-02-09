0f84a792ca5ca1b14a30b25b215ee81bfbd3989f
Kevin Pak <kedpak@yahoo.com>
2018-02-09 01:36:47 -0800

    Add changes to configuration

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ app.get('/', (req, res) => {
 });
 
 // Use port provided by Heroku or use port 5000
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+app.listen(process.env.PORT || 5000, "0.0.0.0", function() {
+	console.log("Listening on Port");
+});
