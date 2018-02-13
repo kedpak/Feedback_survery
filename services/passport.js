3e77f95017d821e67951ebc50b42cc8739378ac3
Kevin Pak <kedpak@yahoo.com>
2018-02-13 10:23:28 -0800

    Update documentation inside passport.js

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 const GoogleStrat = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+// A mongoose model class with users collection
+const User = mongoose.model('users');
 
 // Create new instance of google passport strategy. Allows users to authenticate
 // through google.
@@ -10,11 +14,11 @@ passport.use(
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback'
-		}, 
+		},
+		// Callback gets called when user profile is sent back to user from google.
+		// Creats new User mongoose instance and saves google id inside db
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access token: ', accessToken);
-			console.log('refresh token: ', refreshToken);
-			console.log('profile: ', profile);
+			new User({ googleId: profile.id}).save();
 		}
 	)
 );
