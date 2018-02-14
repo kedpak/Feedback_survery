59edde2a5512fdd0ff0e36e41b57b06745318277
Kevin Pak <kedpak@yahoo.com>
2018-02-13 17:59:35 -0800

    Implement additional rutes for authentication, logout and access if current user exists

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,9 +7,17 @@ module.exports = app => {
 		})
 	);
 
-	// Route which exchanges code to recieve user profile. Puts user on hold until request is sent. 
+	// Route which exchanges code to recieve user profile. Puts user on hold until request is sent.
 	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	// Logs user out. Takes cookie and kills id inside cookie.
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
+	// User can now get access to user through authentication
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
 };
-
-
-
