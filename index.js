c831c5aad94e22dac5ca6f90f42ec6bc99f6bf6a
Kevin Pak <kedpak@yahoo.com>
2018-02-13 17:58:27 -0800

    Add cookie initializer and connect to app.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,17 @@ require('./services/passport');
 // Connect to mongo db
 mongoose.connect(keys.mongoDBuri);
 const app = express();
+
+// Create cookie session for user to send to server to authenticate user.
+app.use(
+	cookieSession({
+			maxAge: 30 * 24 * 60 * 1000,
+			keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Call authroutes function (from routes/authRoutes) with app argument.
 require('./routes/authRoutes')(app);
