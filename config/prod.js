05594ad699d42a97b0aa315cb7021b6a1fe455f1
Kevin Pak <kedpak@yahoo.com>
2018-02-16 15:21:32 -0800

    Add envioronmental varibles for Strip API.

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     mongoDBuri: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE
+    cookieKey: process.env.COOKIE,
+    stripePubkey: process.env.STRIPE_PUB_KEY,
+    stripeSecretkey: process.env.STRIPE_SECRET_KEY
 }
