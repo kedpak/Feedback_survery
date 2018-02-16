eb0020a7afb8643f3039d5899b5fa5912d29d8ab
Kevin Pak <kedpak@yahoo.com>
2018-02-15 21:53:49 -0800

    Implement login and logout utilitiies, and handle dashboard change based on login status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
-  // Header of application. 
+  // Header of application.
+
+  // Checks user login status.
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google Account</a>
+          </li>
+        )
+      default:
+        return <li><a href="/api/logout">Logout</a></li>
+    }
+  }
   render() {
     return (
       <nav>
          <div className="nav-wrapper">
            <a className="left brand-logo">Feedback Survey</a>
            <ul id="nav-mobile" className="right">
-             <li><a>Login</a></li>
-             <li><a href="badges.html">Components</a></li>
-             <li><a href="collapsible.html">JavaScript</a></li>
+             {this.renderContent()}
            </ul>
          </div>
       </nav>
@@ -18,4 +33,9 @@ class Header extends Component {
   }
 }
 
-export default Header;
+// Calls entire state object out of redux store
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+// Connects Header component to the Store.
+export default connect(mapStateToProps)(Header);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,12 +8,19 @@ module.exports = app => {
 	);
 
 	// Route which exchanges code to recieve user profile. Puts user on hold until request is sent.
-	app.get('/auth/google/callback', passport.authenticate('google'));
+	app.get(
+		'/auth/google/callback',
+		passport.authenticate('google'),
+		(req, res) => {
+			// If google authentication is completed, Redirects to surveys page.
+			res.redirect('/surveys');
+		}
+	);
 
-	// Logs user out. Takes cookie and kills id inside cookie.
+	// Logs user out. Takes cookie and kills id inside cookie. Redirect back to initial dashboard.
 	app.get('/api/logout', (req, res) => {
 		req.logout();
-		res.send(req.user);
+		res.redirect('/');
 	});
 
 	// User can now get access to user through authentication
