526900aedeba87dd7f669cd3e561107ce723c105
Kevin Pak <kedpak@yahoo.com>
2018-02-17 14:34:27 -0800

    Implement handling of credits being added to user property.

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,14 +3,16 @@ const stripe = require('stripe')(keys.stripeSecretkey);
 
 // Posts submitted payment data to api and charges user for credit card payment.
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-    stripe.charges.create({
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
         amount: 500,
         currency: "usd",
         source: req.body.id, // obtained with Stripe.js
         description: "Charge card for user."
-      }, function(err, charge) {
-        // asynchronously called
       });
+      // Adds 5 credits to userCredits property for return User object.
+      req.user.userCredits += 5;
+      // Save is async so after save is complete, updated user model is returned.
+      const user = await req.user.save();
   });
 };
