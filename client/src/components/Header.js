df11878182dbe7f076e243ad3c32f38452efe952
Kevin Pak <kedpak@yahoo.com>
2018-02-17 15:31:29 -0800

    Implement amount of credits to be shown inside of header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,10 @@ class Header extends Component {
       default:
         return [
           <li key="1"><Payments /></li>,
-          <li key="2"><a href="/api/logout">Logout</a></li>
+          <li key="2" style={{margin: '0 10px'}}>
+            Credits: {this.props.auth.userCredits}
+          </li>,
+          <li key="3"><a href="/api/logout">Logout</a></li>
         ]
     }
   }
