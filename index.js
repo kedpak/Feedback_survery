a42f2ea019128e602cd6b9350e308664587e0c61
Kevin Pak <kedpak@yahoo.com>
2018-02-16 19:59:12 -0800

    Import route for payments functionality.

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use(passport.session());
 
 // Call authroutes function (from routes/authRoutes) with app argument.
 require('./routes/authRoutes')(app);
+// Call paymentRoutes function with app argument.
+require('./routes/paymentRoutes')(app);
 
 // Use port provided by Heroku or use port 5000
 app.listen(process.env.PORT || 5000, "0.0.0.0", function() {
