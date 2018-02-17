0f10265b0524b1c9fed8244990c4d93ee8e05d97
Kevin Pak <kedpak@yahoo.com>
2018-02-16 22:47:07 -0800

    Implement stripe credit card charger when user submits billing info. Posts info to express api.

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,16 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretkey);
 
+// Posts submitted payment data to api and charges user for credit card payment.
 module.exports = app => {
   app.post('/api/stripe', (req, res) => {
+    stripe.charges.create({
+        amount: 500,
+        currency: "usd",
+        source: req.body.id, // obtained with Stripe.js
+        description: "Charge card for user."
+      }, function(err, charge) {
+        // asynchronously called
+      });
   });
 };
