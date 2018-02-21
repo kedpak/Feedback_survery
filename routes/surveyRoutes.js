5875b2e66aed7a48e67d7410568245cc8acaaabc
Kevin Pak <kedpak@yahoo.com>
2018-02-20 20:09:36 -0800

    Instantiate mailer class after new survey model has been created.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const checkCredits = require('../middlewares/checkCredits');
-
+const Mailer = require('../services/mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
@@ -17,5 +18,9 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    // Initialize Mailer with survey arguments and template. 
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
   });
 };
