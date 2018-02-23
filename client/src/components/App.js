b9c5cc78fdc7751def3a48a1a6818251d58197a9
Kevin Pak <kedpak@yahoo.com>
2018-02-22 20:28:46 -0800

    Refactor module to utilize new survey component.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import  { connect } from 'react-redux';
 import * as actions from '../actions';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
-const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
   componentDidMount() {
