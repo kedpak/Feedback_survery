a83c15adb0b7e41dda34821f99d89a70c95b8249
Kevin Pak <kedpak@yahoo.com>
2018-02-24 22:13:45 -0800

    Implement all four fields into form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import _ from 'lodash';
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title'},
+  { label: 'Subject line', name: 'subject'},
+  { label: 'Email Body', name: 'body'},
+  { label: 'Recipient List', name: 'emails'}
+];
 // Shows form for user to add comonent.
 class SurveyForm extends Component {
   renderFields(){
-    return (
-      <div>
-        <Field type="text" name="title" component={SurveyField} />
-      </div>
-    )
+    return _.map(FIELDS, ({ label, name }) => {
+      return <Field component={SurveyField} type="text" label={label} name={name} key={name}/>
+    })
   }
   render() {
     return (
