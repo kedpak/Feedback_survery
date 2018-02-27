556872b46f6719b7879ebe548851336c04406bd9
Kevin Pak <kedpak@yahoo.com>
2018-02-27 14:36:46 -0800

    replace regex for email check.

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,6 +1,6 @@
 // Iterates through all recipients user has inputted, and splits at comma.
 
-const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export default (recipients) => {
   const invalidEmails = recipients.split(',')
