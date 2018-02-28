3de1abc2f32e301cf1e068dc8252cac0207984aa
Kevin Pak <kedpak@yahoo.com>
2018-02-27 17:41:43 -0800

    Global constant which contains list of object of form fields. Implemented to refactor reusage of the fields object in both the survey form and form review.

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,7 @@
+// Form fields.
+export default [
+  { label: 'Survey Title', name: 'title', errMsg: 'Please provide a title!'},
+  { label: 'Subject line', name: 'subject', errMsg: 'Please provide a subject!'},
+  { label: 'Email Body', name: 'body', errMsg: 'Please provide a body!'},
+  { label: 'Recipient List', name: 'emails', errMsg: 'Please provide a list of emails!'}
+];
