d4b84896bf560960d5a76cefe7986c790307569b
Kevin Pak <kedpak@yahoo.com>
2018-02-27 18:00:42 -0800

    Complete review form to display all fields with inputted data.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
+import _ from 'lodash';
 
 const SurveyReview = ({ onCancel, formValues }) => {
+
+  const fieldList = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Confirm your entries!</h5>
-      
+      {fieldList}
       <button
         className="yellow darken-3 btn-flat"
         onClick={onCancel}
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
+
 // SurveyNew displays the Survey Form and and the review of survey.
 class SurveyNew extends Component {
   state = {
     showReview: false
   };
 
-  // Render form if showReview is false. Otherwise show survey form. 
+  // Render form if showReview is false. Otherwise show survey form.
   renderContent() {
     if (this.state.showReview) {
       return (<SurveyFormReview
